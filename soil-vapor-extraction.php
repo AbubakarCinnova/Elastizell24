6507ed9502a69604060ba2e9604db36998988af6
AbubakarCinnova <amahboob@cinnova.com>
2024-09-24 15:14:44 +0500

    Create soil-vapor-extraction.php

---
+++ soil-vapor-extraction.php
@@ -0,0 +1,192 @@
+<?php include('header.php'); ?>
+<title>Soil Vapor Extraction and Thermal Remediation with Lightweight Cellular Concrete | Elastizell Systems</title>
+
+<meta name="description"
+    content=" Explore Elastizell Systems' expertise in Soil Vapor Extraction (SVE) and Thermal Remediation using Lightweight Cellular Concrete. Our advanced LCC solutions enhance the efficiency">
+
+</head>
+
+<body>
+    <div id=" wrapper">
+        <div class="float-text show-on-scroll">
+            <span><a href="#">Scroll to top</a></span>
+        </div>
+        <div class="scrollbar-v show-on-scroll"></div>
+
+        <?php include('menu.php'); ?>
+
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+            <!-- section begin -->
+            <section id="subheader" class="jarallax text-light">
+                <img src="images/background/" class="jarallax-img" alt="">
+                <div class="container relative z-index-1000">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="subtitle s2 bg-color text-light wow fadeInUp mb-2">Our Service</div>
+                            <h1>Soil Vapor Extraction
+                            </h1>
+                            <ul class="crumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">Soil Vapor Extraction
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="de-overlay"></div>
+            </section>
+            <!-- section close -->
+
+            <div class="bg-color relative z-index-1000 mt-40 mb40">
+                <a href="contact.php">
+                    <img src="images/svg/phone-calling-svgrepo-com.svg"
+                        class="border-white-6 fs-30 bg-color w-80px p-3 mb20 circle absolute abs-center sm-hide" alt="">
+                </a>
+            </div>
+
+            <section>
+                <div class="container">
+                    <div class="row g-5">
+                        <div class="col-lg-12">
+                            <div class="row g-4">
+                                <div class="col-lg-12">
+
+                                    <!-- content start here -->
+
+
+
+
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="spacer-double"></div>
+
+                            <div class="row g-4">
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".0s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4>Expertise You Can Trust
+                                            </h4>
+                                            <p class="mb-0">Decades of experience in providing innovative remediation
+                                                solutions using Lightweight Cellular Concrete (LCC) for Soil Vapor
+                                                Extraction (SVE) and thermal remediation projects.
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".2s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4> Tailored LCC Solutions
+
+                                            </h4>
+                                            <p class="mb-0">Custom LCC mixes are designed to enhance SVE efficiency and
+                                                provide superior thermal insulation, ensuring effective contaminant
+                                                removal and long-term stability.
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".4s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4>Environmental Engineering Excellence
+                                            </h4>
+                                            <p class="mb-0">Our environmental engineers leverage their expertise to
+                                                deliver precise and durable solutions for complex remediation projects.
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".6s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4>Seamless Project Management
+
+                                            </h4>
+                                            <p class="mb-0">From planning to completion, we manage SVE and thermal
+                                                remediation projects with precision, ensuring timely delivery and
+                                                adherence to budget.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".8s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4>Durable, High-Quality Products
+                                            </h4>
+                                            <p class="mb-0">We use top-quality Lightweight Cellular Concrete to support
+                                                long-lasting and effective remediation efforts, providing reliable
+                                                containment and insulation.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay="1s">
+                                    <div>
+                                        <i
+                                            class="bg-color text-light fs-48 p-2 absolute id-color icon_check rounded-10px"></i>
+                                        <div class="pl-80">
+                                            <h4>Versatile Applications
+                                            </h4>
+                                            <p class="mb-0">Adaptable LCC solutions for Soil Vapor Extraction (SVE),
+                                                in-situ thermal remediation, and complex environmental projects across
+                                                diverse site conditions.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="spacer-double"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="bg-color text-light pt40 pb40">
+                <div class="container">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-lg-9">
+                            <h3 class="mb-1"><i class="icofont-check-circled fs-32 me-3 id-color-2"></i>Elastizell
+                                Systems</h3>
+                        </div>
+
+                        <div class="col-lg-3 text-lg-end text-start">
+                            <a class="btn-main bg-color-2" href="contact.php">Schedule Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- content close -->
+
+
+        <?php include('footer.php'); ?>
